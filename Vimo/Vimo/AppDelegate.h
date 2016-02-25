e6a9555526b7c1add768bae249d1aa9931070a1c
charles <charleshkang1@gmail.com>
2016-02-25 02:59:36 -0500

    updated readme, added xcode project, and added gitignore

---
+++ Vimo/Vimo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vimo
+//
+//  Created by Charles Kang on 2/25/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vimo/Vimo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vimo
+//
+//  Created by Charles Kang on 2/25/16.
+//  Copyright © 2016 Charles Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
