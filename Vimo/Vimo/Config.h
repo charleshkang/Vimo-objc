74891a7a5333ed96ca9e3d3547b26fb3865a6574
charles <charleshkang1@gmail.com>
2016-05-05 00:33:17 -0400

    reminder to set up heroku server to refresh session tokens again...

--- Vimo/Vimo/Config.h
+++ Vimo/Vimo/Config.h
@@ -11,7 +11,7 @@
 
 #define kClientId "d5b5e89f88e146bbbeb0ad1375934f62"
 #define kCallbackURL "vimo://"
-
+// delete me later
 #define kTokenSwapServiceURL "https://vimo.herokuapp.com/swap"
 #define kTokenRefreshServiceURL "https://vimo.herokuapp.com/refresh"
 
